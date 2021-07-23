9a71b4dcc712b81770c4f65462f935cdfbd4d991
jeondoh <41669026+jeondoh@users.noreply.github.com>
2021-07-24 00:14:38 +0900

    코딩테스트 연습 > 2021 KAKAO BLIND RECRUITMENT > 순위검색

--- 01_programers/SearchRank_kakao.java
+++ 01_programers/SearchRank_kakao.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 // 2021-06-10(목)
 // 코딩테스트 연습 > 2021 KAKAO BLIND RECRUITMENT > 순위검색
-public class kakao_SearchRank {
+public class SearchRank_kakao {
 
     public static void main(String[] args) {
         String[] info = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
