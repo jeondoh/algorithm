d84120dc2ab79d50d936c365efc5e3d4b81838cd
jeondoh <41669026+jeondoh@users.noreply.github.com>
2021-07-24 00:10:32 +0900

    코딩테스트 연습 > 찾아라 프로그래밍 마에스터 > 폰켓몬

--- 01_programers/Maester_Poketmon.java
+++ 01_programers/Maester_Poketmon.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 // 2021-07-25(일)
 // 코딩테스트 연습 > 찾아라 프로그래밍 마에스터 > 폰켓몬
@@ -13,12 +14,13 @@ public class Maester_Poketmon {
     }
 
     public static int solution(int[] nums) {
-        int answer = 0;
-
-
-
-        return answer;
+        int size = nums.length; // 포켓몬 초기 개수(중복포함)
+        int poketmonNum = size / 2; // 선택할 수 있는 포켓몬 수
+        // 배열 중복제거
+        // HashSet / LinkedHashSet OR Stream / Distinct 이용
+        int[] resultArr = Arrays.stream(nums).distinct().toArray(); // 포켓몬 중복제거
+        int resultSize = resultArr.length; // 포켓몬 개수(중복제거)
+        return Math.min(poketmonNum, resultSize);
     }
-
 
 }// end class
