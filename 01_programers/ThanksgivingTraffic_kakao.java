d263a36a541d1bdd6017022f2588bb69d5dd21f6
jeondoh <41669026+jeondoh@users.noreply.github.com>
2021-08-21 23:08:19 +0900

    코딩테스트 연습 > 2018 KAKAO BLIND RECRUITMENT > [1차] 추석 트래픽 (미완)

--- 01_programers/ThanksgivingTraffic_kakao.java
+++ 01_programers/ThanksgivingTraffic_kakao.java
@@ -1,3 +1,4 @@
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -8,8 +9,9 @@ import java.util.Date;
 
 public class ThanksgivingTraffic_kakao {
 
-    public static void main(String[] args) throws ParseException {
+    public static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
+    public static void main(String[] args) throws ParseException {
         String[] lines1 = {
                 "2016-09-15 01:00:04.001 2.0s",
                 "2016-09-15 01:00:07.000 2s"
@@ -30,37 +32,87 @@ public class ThanksgivingTraffic_kakao {
                 "2016-09-15 21:00:00.966 0.381s",
                 "2016-09-15 21:00:02.066 2.62s"
         };
-
-        String seconds = "0.351";
-//        float resultSeconds =
-//        System.out.println(resultSeconds);
-
 //        System.out.println(solution(lines1));
-//        System.out.println(solution(lines2));
+        System.out.println(solution(lines2));
 //        System.out.println(solution(lines3));
     }
 
     public static int solution(String[] lines) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
         int size = lines.length;
-        long[] startTime = new long[size];
-        long[] endTime = new long[size];
+        long[] startTimeArr = new long[size];
+        long[] endTimeArr = new long[size];
 
+        // 배열내 시간 계산 > 시작시간 & 종료시간 나눠서 저장 (최대 2000번 돌림 ㅠㅠ)
         for(int i = 0; i<size; i++){
             String[] spData = lines[i].split(" ");
             String date = spData[0] + " " + spData[1];
             String seconds = spData[2].replace("s", "");
-            long resultSeconds = (long) (Long.parseLong(seconds) + 0.001f);
+            float resultSeconds = Float.parseFloat(seconds) - 0.001f;
 
-            endTime[i] = sdf.parse(date).getTime();
+            long endTime = sdf.parse(date).getTime();
+            long startTime = endTime - (long)(resultSeconds * 1000);
+
+            startTimeArr[i] = startTime;
+            endTimeArr[i] = endTime;
         }
+        // 배열 내 제일 작은 시간
+        long minTime = Arrays.stream(startTimeArr).min().getAsLong();
+        // 배열 내 제일 큰 시간
+        long maxTime = Arrays.stream(endTimeArr).max().getAsLong();
+        // 제일 큰시간 - 제일 작은시간 > 시차(초)를 구함
+        int diffTime = (int)Math.ceil((maxTime - minTime) / 1000.000f) + 1;
+        // 매 초를 비교하기 위해 시작시간
+        long compStartTime = minTime;
+        // 매 초를 비교하기 위해 종료시간(1초(1000밀리초)를 더한 값 저장 변수)
+        long compEndTime = minTime + 999L;
+        // 구간 최대 처리량
+        int max = 0;
 
-//        long diff = sDate2.getTime() - sDate.getTime();
+        // 매초 시간 비교
+        for(int i=0; i<=diffTime; i++){
+            // 처리량
+            int cnt = 0;
 
-        long min = Arrays.stream(startTime).min().getAsLong();
-        System.out.println(min);
+            for(int j=0; j<size; j++){
+                if (betweenTime(compStartTime, compEndTime, startTimeArr[j], endTimeArr[j])) {
+                    System.out.println("포함");
+                    cnt++;
+                }
+            }
+            System.out.println("END FOR DIFFTIME");
+            // 비교를 위해 1초(1000밀리초)씩 증가
+            compStartTime += 1000L;
+            compEndTime += 999L;
+            // 구간 최대 처리량 비교
+            max = Math.max(max, cnt);
+        }
+        return max;
+    }
 
-        return 0;
+    public static boolean betweenTime(long compStartTime, long compEndTime, long startTime, long endTime){
+        boolean bStartTime;
+        boolean bEndTime;
+
+        System.out.println("=============================================================");
+        System.out.println("비교 시작시간 :: " + sdf.format(compStartTime));
+        System.out.println("비교 종료시간 :: " + sdf.format(compEndTime));
+        System.out.println("시작시간 :: " + sdf.format(startTime));
+        System.out.println("종료시간 :: " + sdf.format(endTime));
+        System.out.println("=============================================================");
+
+        // 시작시간 비교
+        if(startTime >= compStartTime){
+            bStartTime = compEndTime > startTime;
+        }else{
+            bStartTime = endTime > compStartTime;
+        }
+        // 종료시간 비교
+        if(endTime >= compEndTime){
+            bEndTime = startTime <= compEndTime && endTime > compEndTime;
+        }else{
+            bEndTime = compStartTime <= endTime;
+        }
+        return bStartTime || bEndTime;
     }
 
 }// end class
