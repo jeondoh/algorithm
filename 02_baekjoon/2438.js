e1e3dc1aa885c3c29c75bc2bd85d97ea0c264005
jundoh <jundo1302@gmail.com>
2022-01-27 21:53:36 +0900

    https://www.acmicpc.net/problem/2438

---
+++ 02_baekjoon/2438.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().split('\n');
+
+let n = Number(input[0]);
+let result = "";
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j <= i; j++) {
+    result += "*"
+  }
+  result += "\n"
+}
+
+console.log(result);
