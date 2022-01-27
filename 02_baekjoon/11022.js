ed6bcccd92f223db2f81ab8d649ed8d02e7ec680
jundoh <jundo1302@gmail.com>
2022-01-28 02:27:31 +0900

    https://www.acmicpc.net/problem/11022

---
+++ 02_baekjoon/11022.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync("./dev/stdin").toString().split('\n');
+const arr = Number(input[0]);
+let result = "";
+
+for (let i = 1; i <= arr; i++) {
+  const arrNums = input[i].split(" ");
+  const a = Number(arrNums[0]);
+  const b = Number(arrNums[1]);
+  result += `Case #${i}: ${a} + ${b} = ${a + b}\n`;
+}
+console.log(result);
