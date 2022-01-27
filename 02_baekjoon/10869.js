b53e0d280c5b790637392421d0b11adb17e7e299
jundoh <jundo1302@gmail.com>
2022-01-27 21:54:04 +0900

    https://www.acmicpc.net/problem/10869

---
+++ 02_baekjoon/10869.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const result = input[0].split(" ");
+const a = Number(result[0]);
+const b = Number(result[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Number(a / b));
+console.log(a % b);
